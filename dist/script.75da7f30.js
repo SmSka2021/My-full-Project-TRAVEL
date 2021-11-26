38b74f9f6f82b88bea9d6c5633c89c140ee3b043
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2021-11-26 12:23:27 +0300

    button hover

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63913" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53601" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
